c136789f3de66ce48a524ace7dde01371c6a3cad
Venkatasivareddy B <venkatasivareddyb09@gmail.com>
2026-03-11 17:34:15 +0530

    Execise1_Assignment1_Csharp

---
+++ Assignment1_Csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Transactions;
+
+namespace Assignment1_Csharp
+{
+    internal class Program
+    {
+        static void Execise1()
+        {
+            Console.WriteLine("Enter NUmber 1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter NUmber 2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Quotient:{ num2 / num1}");
+        }
+        static void Main(string[] args)
+        {
+            Execise1();
+        }
+    }
+}
