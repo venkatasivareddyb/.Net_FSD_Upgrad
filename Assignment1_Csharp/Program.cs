c4c4730434c50e6b4d920d730581c0df235bc2fb
Venkatasivareddy B <venkatasivareddyb09@gmail.com>
2026-03-11 17:47:19 +0530

    Execise2

--- Assignment1_Csharp/Program.cs
+++ Assignment1_Csharp/Program.cs
@@ -12,6 +12,13 @@ namespace Assignment1_Csharp
             int num2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Quotient:{ num2 / num1}");
         }
+        static void Execise2()
+        { 
+        Console.WriteLine("Enter Kilometers:");
+        int km = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Meters:{ km * 1000}m");
+        }
+
         static void Main(string[] args)
         {
             Execise1();
